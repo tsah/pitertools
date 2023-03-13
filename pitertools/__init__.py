783c8e0a00f28577ad3ba878032d77fe78e88eaa
tsah-alike <tsah@alike.health>
2023-03-13 15:52:03 +0200

    Refactor to classes

--- pitertools/__init__.py
+++ pitertools/__init__.py
@@ -1,4 +1,4 @@
-from typing import Callable, Iterator, Tuple, TypeVar
+from typing import Any, Callable, Generic, Iterator, List, Optional, Tuple, TypeVar
 from threading import Event, Lock, Thread
 from queue import Queue
 
@@ -23,69 +23,105 @@ def map_parallel(
     If any thread experiences an exception, all threads shutdown gracefully after finishing
     current computation.
     """
-    lock = Lock()
-    results_queue = Queue(maxsize=num_workers)
-    thread_stop_events = []
-    enumerated_it = enumerate(it)
-    for i in range(num_workers):
-        stop_event = Event()
-        t = Thread(
-            target=_task,
-            kwargs={
-                'it': enumerated_it,
-                'func': func,
-                'lock': lock,
-                'i': i,
-                'verbose': verbose,
-                'results_queue': results_queue,
-                'stop_event': stop_event
-            }
-        )
-        t.start() 
-        thread_stop_events.append(stop_event)
-    finished = 0
-    error = None
-    current_index = 0
-    stored_items = []
-    while True:
-        if finished == num_workers:
-            if error:
-                raise error
-            else:
+    return _ParallelMap(func, it, num_workers, ordered, verbose).start()
+
+
+class _ParallelMap(Generic[T, G]):
+    def __init__(
+        self,
+        func: Callable[[T], G],
+        it: Iterator[T],
+        num_workers: int,
+        ordered: bool,
+        verbose: bool
+    ) -> None:
+        # params
+        self.func = func
+        self.it = enumerate(it)
+        self.num_workers = num_workers
+        self.ordered = ordered
+        self.verbose = verbose
+        # state
+        self.finished_workers: int = 0
+        self.error: Optional[Exception] = None
+        self.current_index: int = 0
+        self.stored_items: List[Any] = []
+        self.lock = Lock()
+        self.results_queue = Queue(maxsize=num_workers)
+        self.thread_stop_events = []
+
+    def start(self) -> Iterator[G]:
+        self._start_workers()
+        while True:
+            if self._is_finished():
+                self._raise_if_needed()
                 break
-        current = results_queue.get(block=True)
-        if type(current) == _End:
-            finished += 1
-            continue
-        if type(current) == _Error:
-            finished += 1
-            error = current.e
-            for event in thread_stop_events:
-                event.set()
-            continue
-        item_index = current[0]
-        item = current[1]
-        if ordered:
-            if item_index == current_index:
-                yield item
-                current_index += 1
-                while True:
-                    if not stored_items:
-                        break
-                    if stored_items[-1][0] == current_index:
-                        current_item = stored_items.pop()[1]
-                        yield current_item
-                        current_index += 1
-                    else:
-                        break
+            current = self.results_queue.get(block=True)
+            if type(current) == _End:
+                self.finished_workers += 1
+            elif type(current) == _Error:
+                self._error(current.e)
             else:
-                _insort(stored_items, current, key=lambda current: -current[0])
-        else:
-            yield item
+                if self.ordered:
+                    self._store(current)
+                    yield from self._flush_stored()
+                else:
+                    yield current[1]
+
+    def _start_workers(self) -> None:
+        for i in range(self.num_workers):
+            stop_event = Event()
+            task = _Worker(
+                it=self.it,
+                func=self.func,
+                lock=self.lock,
+                t_id=i,
+                verbose=self.verbose,
+                results_queue=self.results_queue,
+                stop_event=stop_event
+            )
+            thread = Thread(
+                target=task.run
+            )
+            thread.start() 
+            self.thread_stop_events.append(stop_event)
+
+    def _stop_all_tasks(self) -> None:
+        for event in self.thread_stop_events:
+            event.set()
+
+    def _is_finished(self) -> bool:
+        return self.num_workers == self.finished_workers
+
+    def _raise_if_needed(self) -> None:
+        if self.error:
+            raise self.error
+
+    def _flush_stored(self) -> List[G]:
+        res = []
+        while True:
+            if not self.stored_items:
+                return res
+            if self.stored_items[-1][0] == self.current_index:
+                current_item = self.stored_items.pop()[1]
+                res.append(current_item)
+                self.current_index += 1
+            else:
+                return res
+
+    def _store(self, item: Tuple[int, G]) -> None:
+        _insort(self.stored_items, item, key=lambda item: -item[0])
+
+    def _error(self, e: Exception) -> None:
+        self.finished_workers += 1
+        self.error = e
+        self._stop_all_tasks()
+
 
 def _insort(a, x, key):
     lo = _bisect_right(a, x, key=key)
     a.insert(lo, x)
+
 
 def _bisect_right(a, x, key) -> int:
     """Return the index where to insert item x in list a, assuming a is sorted.
@@ -104,39 +140,55 @@ def _bisect_right(a, x, key) -> int:
     return lo
 
 
-def _task(
-    it: Iterator[Tuple[int,T]],
-    func: Callable[[T], G],
-    lock: Lock,
-    i: int,
-    verbose: bool,
-    results_queue: Queue,
-    stop_event: Event
-) -> None:
-    if verbose:
-        print(f"Worker {i} is starting")
-    while True:
-        if stop_event.is_set():
-            print(f"Worker {i} Stopped")
-            results_queue.put(_End(), block=True)
-        lock.acquire(blocking=True)
-        try:
-            current = next(it)
-        except StopIteration:
-            if verbose:
-                print(f"Worker {i} finished")
-            results_queue.put(_End(), block=True)
-            break
-        finally:
-            lock.release()
-        try:
-            transformed = func(current[1])
-        except Exception as e:
-            if verbose:
-                print(f"worker {i} failed with exception {e}")
-            results_queue.put(_Error(e), block=True)
-            break
-        results_queue.put((current[0], transformed), block=True)
+class _Worker(Generic[T, G]):
+    
+    def __init__(
+        self,
+        it: Iterator[Tuple[int,T]],
+        func: Callable[[T], G],
+        lock: Lock,
+        t_id: int,
+        verbose: bool,
+        results_queue: Queue,
+        stop_event: Event
+    ) -> None:
+        self.it = it
+        self.func = func
+        self.lock = lock
+        self.t_id = t_id
+        self.verbose = verbose
+        self.results_queue = results_queue
+        self.stop_event = stop_event
+        
+    def run(self) -> None:
+        self._print(f"Worker {self.t_id} is starting")
+        while True:
+            if self.stop_event.is_set():
+                self._print(f"Worker {self.t_id} stopped")
+                self._send(_End())
+            self.lock.acquire(blocking=True)
+            try:
+                current = next(self.it)
+            except StopIteration:
+                self._print(f"Worker {self.t_id} finished")
+                self._send(_End())
+                break
+            finally:
+                self.lock.release()
+            try:
+                transformed = self.func(current[1])
+            except Exception as e:
+                self._print(f"worker {self.t_id} failed with exception {self.t_id}")
+                self._send(_Error(e))
+                break
+            self._send((current[0], transformed))
+
+    def _print(self, msg: str) -> None:
+        if self.verbose:
+            print(msg)
+
+    def _send(self, item: Any) -> None:
+        self.results_queue.put(item, block=True)
 
 
 class _Error:
